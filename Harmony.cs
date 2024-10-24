bba181424a293718767e44533cfcc99675943224
Digitalzombie <blackdevil32@gmx.net>
2024-10-24 14:02:04 +0200

    Add project files.

---
+++ Harmony.cs
@@ -0,0 +1,121 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Il2CppInterop;
+using Il2CppInterop.Runtime.Injection; 
+using System.Collections;
+using Il2Cpp;
+using Il2CppNewtonsoft.Json.Linq;
+
+
+namespace LitharsMending
+{   
+    [HarmonyLib.HarmonyPatch(typeof(Panel_Inventory_Examine), "Enable", new Type[] { typeof(bool), typeof(ComingFromScreenCategory) })]
+    public class Panel_Inventory_Examine_Enable_Patch
+    {
+        public static void Postfix(ref Panel_Inventory_Examine __instance, bool enable, ComingFromScreenCategory cameFrom)
+        {
+            if (Settings.options.modEnabled && LitharsMendingMain.buttonRepairMain != null)
+            {
+                if(__instance.m_GearItem && __instance.m_GearItem.m_ClothingItem && __instance.m_GearItem.CurrentHP == 0)
+                {
+                    LitharsMendingMain.buttonRepairMain.SetActive(enable);
+                    LitharsMendingMain.buttonRepairMain.transform.localPosition = new Vector3(0, -60, 0);
+                }                
+            }           
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(Panel_Inventory_Examine), "OnSelectRepairButton")]
+    public class OnSelectRepairButton_ClickButtonPatch
+    {
+        public static void Postfix(ref Panel_Inventory_Examine __instance)
+        {
+            if (Settings.options.modEnabled && LitharsMendingMain.buttonRepairMain != null)
+            {
+                if (__instance.m_GearItem && __instance.m_GearItem.m_ClothingItem && __instance.m_GearItem.CurrentHP == 0)
+                {
+                    LitharsMendingMain.repairPanel.SetActive(true);
+                    LitharsMendingMain.repairPanelButtons.SetActive(true);
+
+                    float chanceSuccessfullRepair = __instance.GetChanceSuccessfullRepair(__instance.m_GearItem);
+                    float chanceActionSuccess = __instance.GetChanceActionSuccess(chanceSuccessfullRepair);
+                    Repairable repairable = __instance.m_GearItem.m_Repairable;
+                    string expandedDurationString = Utils.GetExpandedDurationString(__instance.GetModifiedRepairDuration(repairable, repairable.m_DurationMinutes));
+
+                    __instance.m_Repair_AmountLabel.text = __instance.GetConditionIncreaseFromRepair(__instance.m_GearItem) + "%";
+                    __instance.m_Repair_ChanceSuccessLabel.text = chanceActionSuccess.ToString("F0") + "%";
+                    __instance.m_Repair_ConditionCapLabel.text = repairable.m_RepairConditionCap.ToString("F0") + "%";
+                    __instance.m_Repair_TimeLabel.text = expandedDurationString;
+                    __instance.UpdateWeightAndConditionLabels();
+                }
+            }
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(Panel_Inventory_Examine), "GetChanceSuccessfullRepair", new Type[] { typeof(GearItem) })]
+    public class GetChanceSuccessfullRepair_Patch
+    {
+        public static void Postfix(ref Panel_Inventory_Examine __instance, ref GearItem gi, ref float __result)
+        {
+            if (Settings.options.modEnabled)
+            {
+                if (__instance.m_GearItem && __instance.m_GearItem.m_ClothingItem && __instance.m_GearItem.CurrentHP == 0)
+                {
+                    if (Settings.options.difficultyPenaltyEnabled && __instance.m_GearItem.m_Repairable)
+                    {
+                        if (Settings.options.difficultyPenalty < 100)
+                        {
+                            int temp = 100 - Settings.options.difficultyPenalty;
+                            __result = __result * (temp / 100);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //int GetModifiedRepairDuration(Repairable r, int baseMinutes)
+    [HarmonyLib.HarmonyPatch(typeof(Panel_Inventory_Examine), "GetModifiedRepairDuration", new Type[] { typeof(Repairable), typeof(int) })]
+    public class GetModifiedRepairDuration_Patch
+    {
+        public static void Postfix(ref Panel_Inventory_Examine __instance, ref Repairable r, ref int baseMinutes, ref int __result)
+        {
+            if (Settings.options.modEnabled)
+            {
+                if (__instance.m_GearItem && __instance.m_GearItem.m_ClothingItem && __instance.m_GearItem.CurrentHP == 0)
+                {
+                    if (Settings.options.timePenaltyEnabled && __instance.m_GearItem.m_Repairable)
+                    {
+                        if(Settings.options.timePenalty > 100)
+                        {
+                            __result = (int)Math.Round(__result * (Settings.options.timePenalty / 100f));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    [HarmonyLib.HarmonyPatch(typeof(Panel_Inventory_Examine), "GetConditionIncreaseFromRepair", new Type[] { typeof(GearItem) })]
+    public class GetConditionIncreaseFromRepair_Patch
+    {
+        public static void Postfix(ref Panel_Inventory_Examine __instance, ref GearItem gi, ref float __result)
+        {
+            if (Settings.options.modEnabled)
+            {
+                if (__instance.m_GearItem && __instance.m_GearItem.m_ClothingItem && __instance.m_GearItem.CurrentHP == 0)
+                {
+                    if (Settings.options.conditionPenaltyEnabled && __instance.m_GearItem.m_Repairable)
+                    {
+                        if (Settings.options.conditionPenalty < 100)
+                        {
+                            int temp = 100 - Settings.options.conditionPenalty;
+                            __result = __result * (temp / 100);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+  
---
+++ Settings.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using ModSettings;
+using MelonLoader;
+
+namespace LitharsMending
+{
+    internal class LitharsMendingSettings : JsonModSettings
+    {        
+        [Section("General")]
+     
+        [Name("Enabled")]
+        [Description("Enable/disable complete mod functionality")]
+        public bool modEnabled = true;
+
+
+        [Section("Difficulty Penalty")]
+
+        [Name("Enable")]
+        [Description("Decreases chance of success to repair a ruined item")]
+        public bool difficultyPenaltyEnabled = true;
+
+        [Name("Percentage")]
+        [Description("Decrease of success in percent (relative to default)")]
+        [Slider(0, 99)]
+        public int difficultyPenalty = 50;        
+
+        [Section("Time Penalty")]
+
+        [Name("Enable")]
+        [Description("Increases the time it takes to repair a ruined item")]
+        public bool timePenaltyEnabled = true;
+
+        [Name("Percentage")]
+        [Description("Duration of repair action on ruined item in percent (relative to default)")]
+        [Slider(100, 300)]
+        public int timePenalty = 150;
+
+        [Section("Condition Penalty")]
+
+        [Name("Enable")]
+        [Description("Limits the condition thats regained on repairing a ruined item")]
+        public bool conditionPenaltyEnabled = true;
+
+        [Name("Condition Penalty")]
+        [Description("Decrease condition on ruined item in percent (relative to default)")]
+        [Slider(1, 100)]
+        public int conditionPenalty = 50;
+
+        protected override void OnConfirm()
+        {
+            base.OnConfirm();
+        }
+    }
+
+    internal static class Settings
+    {
+        public static LitharsMendingSettings options;
+
+        public static void OnLoad()
+        {
+            options = new LitharsMendingSettings();
+            options.AddToModSettings("Lithars Mending");
+        }
+    }
+}
